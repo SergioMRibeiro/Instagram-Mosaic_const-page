184e2bd919ee03f610ac5bee000fa0fe7fa9b795
SergioRibeiro <sergio@maeztra.com>
2022-10-21 10:51:41 -0300

    resolvendo rota

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import resolve from 'path'
 
 const app = express()
 
-app.use('/', express.static(resolve(__dirname, '.build')))
+app.use('/', express.static(resolve(__dirname, './build')))
 
 app.listen(process.env.port || 3000, (err) => {
   if (err) {
